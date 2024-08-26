863811f6a6b48bfa021b7cf0cb7eadcb6ba2d037
ishanioanand <ishanioanand1@gmail.com>
2024-08-26 19:12:51 +0000

    #1

--- lab1.py
+++ lab1.py
@@ -4,7 +4,8 @@
 #1. Write a function reverse_and_uppercase(input_str) that takes a string, reverses it, and converts it to uppercase.
 def reverse_and_uppercase(input_str):
     # Your code here
-    pass
+    revStr = input_str[::-1]
+    return (revStr.upper())
 assert reverse_and_uppercase("hello") == "OLLEH"
 assert reverse_and_uppercase("DataStructures") == "SERUTCURTSATAD"
 
