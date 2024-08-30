d3dcad73981f87ea6b0c8ed52de8817005f1fa6d
ishanioanand <ishanioanand1@gmail.com>
2024-08-30 03:57:45 +0000

    total

--- lab1.py
+++ lab1.py
@@ -30,24 +30,61 @@ assert check_sign(0) == "Zero"
 
 #4. Write a function sum_of_evens(numbers) that returns the sum of all even numbers in a list.
 def sum_of_evens(numbers):
-    # Your code here
-    pass
+    total = 0
+    for num in numbers:
+        if num % 2 == 0:
+            total += num
+    return total
+
 
 assert sum_of_evens([1, 2, 3, 4, 5, 6]) == 12
 assert sum_of_evens([7, 8, 9, 10]) == 18
 
 #5. Write a function fibonacci(n) that returns the nth Fibonacci number. Without recursion. Must use loop.
 def fibonacci(n):
-    # Your code here
-    pass
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
 
 assert fibonacci(5) == 5
 assert fibonacci(10) == 55
 
 #6. Write a function parse_and_calculate(expression) that takes a mathematical expression in string format (e.g., "3+5*2-4/2") and returns the result. Handle the operations with the correct order of precedence.
 def parse_and_calculate(expression):
-    # Your code here
-    pass
+  i =0
+  while(i<len(expression)):
+    if expression[i] =='*':
+      a = int(expression[i-1])
+      b = int (expression[i+1])
+      prod = a*b
+      expression = expression[:i-1] + str(prod) + expression [ i+2:]
+      i = i-2
+    elif expression[i] =='/':
+      a = int(expression[i-1])
+      b = int (expression[i+1])
+      prod = a/b
+      expression = expression[:i-1] + str(prod) + expression [ i+2:]
+      i = i-2
+    else:
+      i = i+1
+  sum = int(expression[0])
+  print(expression)
+  for i in range(1,len(expression)):
+    if expression[i] == '+':
+      b = int(expression[i+1])
+      sum +=b
+    elif expression[i] == '-':
+      b = int(expression[i+1])
+      sum -=b
+  print(sum)    
+  return (sum)
 
-assert parse_and_calculate("3+5*2-4/2") == 10
-assert parse_and_calculate("10-3*2+8/4") == 6
+assert parse_and_calculate("3+4*2-4/2") == 9
+assert parse_and_calculate("9-3*2+8/4") == 5
